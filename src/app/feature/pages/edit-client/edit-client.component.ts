32e8d7e669e9fd420d806e394a59bb6582987dd5
pedroprado237 <pedroprado@rede.ulbra.br>
2024-06-30 15:49:14 -0300

    Feature: New component Edit Client

---
+++ src/app/feature/pages/edit-client/edit-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-client',
+  templateUrl: './edit-client.component.html',
+  styleUrl: './edit-client.component.css'
+})
+export class EditClientComponent {
+
+}
