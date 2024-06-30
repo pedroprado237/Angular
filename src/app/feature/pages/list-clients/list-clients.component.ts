a15f52393c84013c60e1eff3d54471b21bc9d600
pedroprado237 <pedroprado@rede.ulbra.br>
2024-06-30 15:49:28 -0300

    Chore: Removing console log

--- src/app/feature/pages/list-clients/list-clients.component.ts
+++ src/app/feature/pages/list-clients/list-clients.component.ts
@@ -12,7 +12,7 @@ export class ListClientsComponent implements OnInit {
   dataClients: any = '';
   dataPagination: any = '';
   pagination: number = 1
-  paginationTotal: number = 0
+  paginationTotal: number = 1
 
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -23,7 +23,6 @@ export class ListClientsComponent implements OnInit {
   loadDataClients() {
     this.authService.dataClientAll()
       .then(response => {
-        console.log("Response: ", response);
         this.dataClients = response?.itens
         this.dataPagination = response?.paginacao;
         this.paginationTotal = Math.ceil(response?.paginacao?.total_registros/20)
